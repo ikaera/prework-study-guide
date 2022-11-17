079b379d71d22b817364127a63e66e23d1d89ff9
ikaera <ikaera@gmail.com>
2022-11-17 13:42:25 -0500

    added script.js file and updated index.html

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// var shape = 'circle';
+// var topics = 'HTML, CSS, Git, JavaScript'
+var topic = 'HTML';
+
+if (topic === 'HTML') {
+  console.log("Let us study HTML!");
+  }else if (topic === 'CSS') {
+    console.log("Let us study CSS!");
+  }else if(topic==='Git') {
+    console.log("Let us study Git!");
+  }else if(topic==='JavaScript'){
+    console.log("Let us study JavaScript");
+  }
+    else {
+      console.log("Please try again!");
+    }
