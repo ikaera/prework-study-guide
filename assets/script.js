e981bb3161cba9afaef62cd5efe133a130193ed5
ikaera <ikaera@gmail.com>
2022-11-17 13:49:48 -0500

    just check

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,29 @@
 // var shape = 'circle';
 // var topics = 'HTML, CSS, Git, JavaScript'
-var topic = 'HTML';
+// var topic = 'HTML';
 
-if (topic === 'HTML') {
-  console.log("Let us study HTML!");
-  }else if (topic === 'CSS') {
-    console.log("Let us study CSS!");
-  }else if(topic==='Git') {
-    console.log("Let us study Git!");
-  }else if(topic==='JavaScript'){
-    console.log("Let us study JavaScript");
-  }
-    else {
-      console.log("Please try again!");
-    }
+// if (topic === 'HTML') {
+//   console.log("Let us study HTML!");
+//   }else if (topic === 'CSS') {
+//     console.log("Let us study CSS!");
+//   }else if(topic==='Git') {
+//     console.log("Let us study Git!");
+//   }else if(topic==='JavaScript'){
+//     console.log("Let us study JavaScript");
+//   }
+//     else {
+//       console.log("Please try again!");
+//     }
+
+//     var shapes = ["triangle", "square", "pentagon", "circle"];
+//     console.log(shapes[0]);
+//     console.log(shapes[1]);
+//     console.log(shapes[2]);
+//     console.log(shapes[3]);
+    
+    
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
